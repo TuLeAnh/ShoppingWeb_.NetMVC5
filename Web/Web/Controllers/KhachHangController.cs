428c2de27713703a1cd79e787661cafb4edbe055
= <=>
2017-12-26 18:11:03 +0700

    add project

---
+++ Web/Web/Controllers/KhachHangController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class KhachHangController : Controller
+    {
+        QLBHEntities db = new QLBHEntities();
+        public ActionResult Index()
+        {
+            var lstKH = from KH in db.KhachHangs select KH;
+            return View(lstKH);
+        }
+	}
+}
